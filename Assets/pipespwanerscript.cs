6f39f91dcec24d342da3098b13ffaee46ac1e4cb
Narjis Fatima <fatima80699@gmail.com>
2026-05-28 20:22:13 -0700

    Adding spawn rate time for pipes to spawn at different time

--- Assets/pipespwanerscript.cs
+++ Assets/pipespwanerscript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class pipespwanerscript : MonoBehaviour
 {
     public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,10 @@ public class pipespwanerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
         Instantiate(pipe, transform.position, transform.rotation);
     }
 }
