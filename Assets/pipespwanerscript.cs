ee399291b15948b0c784c20ff312b0da08a17461
Narjis Fatima <fatima80699@gmail.com>
2026-05-29 20:39:15 -0700

    Making pipe spawn separately at random positions

--- Assets/pipespwanerscript.cs
+++ Assets/pipespwanerscript.cs
@@ -5,10 +5,11 @@ public class pipespwanerscript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2;
     private float timer = 0;
+    public float heightOffset = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
@@ -18,6 +19,16 @@ public class pipespwanerscript : MonoBehaviour
         {
             timer = timer + Time.deltaTime;
         }
-        Instantiate(pipe, transform.position, transform.rotation);
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+    }
+    void spawnPipe() {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x,Random.Range(lowestPoint,highestPoint), 0), transform.rotation);
     }
 }
